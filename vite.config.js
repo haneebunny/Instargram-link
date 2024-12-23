27c7e534e967f21a216117210a8412d0eebaf035
haneebunny <re____@naver.com>
2024-12-24 01:52:39 +0900

    vite 수정

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "dist", // 빌드 결과물 디렉터리
+    outDir: "dist", // 빌드 결과 디렉터리
     rollupOptions: {
-      input: "index.html", // 진입점 파일
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: "", // 공통 SCSS 추가 없이 간단히 사용
+      input: {
+        index: "index.html", // index.html을 번들링 대상에 포함
+        main: "./src/main.js", // main.js를 번들링에 포함
+        loading: "./src/loading.js", // loading.js를 번들링에 포함
+      },
+      output: {
+        entryFileNames: "assets/[name].js", // JS 파일 이름 설정
+        assetFileNames: "assets/[name].[ext]", // 기타 파일 이름 설정 (CSS 등)
       },
     },
   },
